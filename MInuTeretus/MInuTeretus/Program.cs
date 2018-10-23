299ef861a5ebc03b83db41ec437c8a5df202e5ff
Henn Sarv <henn@sarv.ee>
2018-10-23 13:26:33 +0300

    muutsion

---
+++ MInuTeretus/MInuTeretus/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MInuTeretus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ VBKonsool/VBKonsool/Module1.vb
@@ -0,0 +1,10 @@
+﻿Imports System
+Module Module1
+
+    Sub Main()
+        Console.Write("Kes sa oled: ")
+        Console.WriteLine("Tere {0}!", Console.ReadLine())
+
+    End Sub
+
+End Module
