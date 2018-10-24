e46c5ee490a165785e363c41d58ea97875ca7caa
Henn Sarv <henn@sarv.ee>
2018-10-24 12:27:27 +0300

    jutt käis tüüpide teisendamisest

--- Kolmapäev/Kolmapäev/Program.cs
+++ Kolmapäev/Kolmapäev/Program.cs
@@ -10,6 +10,39 @@ namespace Kolmapäev
     {
         static void Main(string[] args)
         {
+            //Console.Write("anna üks arv: ");
+            //string loetud = Console.ReadLine();
+            //int arv = int.Parse(loetud);
+            //Console.WriteLine(arv*arv);
+
+            //Console.Write("millal sa sündinud oled: ");
+            //DateTime sünnipäev = DateTime.Parse(Console.ReadLine());
+            //Console.WriteLine(
+            //    $"sa oled {DateTime.Today.Year - sünnipäev.Year} aastat vana" 
+            //    );
+
+            //double d = 77.77;
+            //Console.WriteLine($"Anna uus arv {d} asemele");
+            //d = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"saime uue arvu {d}");
+
+            int Int = int.MaxValue;   // väga halb komme kasutada muutuja nimeks
+
+            Console.WriteLine(Int);
+
+            long Long = Int;    // andmetüüpi suure tähega
+            Console.WriteLine(Long);
+            Int = (int)(Long*Long);
+            Console.WriteLine($"int on {Int} ruut on {Long*Long}");
+            Long = 4611686014132420609L;
+            unchecked
+            {
+                Int = (int)Long; 
+            }
+            Console.WriteLine(Int);
+
+
+
         }
     }
 }
