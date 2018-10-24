c3fe6f99ad735acc20d7d46f27d59e1b7568caeb
Henn Sarv <henn@sarv.ee>
2018-10-24 10:56:52 +0300

    lisasin kommentaarid

--- MInuTeretus/MInuTeretus/Program.cs
+++ MInuTeretus/MInuTeretus/Program.cs
@@ -22,14 +22,16 @@ namespace MInuTeretus
 
         static void Main(string[] args)
         {
-
-            string nimi = "\tHenn on " + (char)10 +  " \"hea\" \t poiss";
+            // tavaline string \-ga on erimärgid
+            // erimärgid tuleb topeldada
+            string nimi = "\tHenn on \n \"hea\" \t poiss";
 
             Console.WriteLine(nimi);
 
             Console.WriteLine('0'+0);
             Console.WriteLine('\0'+0);
 
+            // see on esceipimata string \ ei ole vaja topeldada
             string failinimi = @"C:\Users\sarvi\Source\Repos\Teispäev\MInuTeretus\MInuTeretus\App.config";
 
             Console.Write("anna üks arv: ");
@@ -37,22 +39,40 @@ namespace MInuTeretus
             int arv = int.Parse(lugesin);
             Console.WriteLine($"lugesin {lugesin} ja sain {arv}");
 
+            // interpoleeritud string e $ string
+            // stringi see on {} vahel avaldised, mis arvutatakse
+            // ja pistetakse vahele
             Console.WriteLine($"Sinu arv oli {arv} ja tema ruut on {arv*arv}");
+
+            // sama tulemus placeholderitega
             Console.WriteLine("sinu arv oli {0} ja tema " +
                 "ruut on {1}", arv, arv*arv);
 
+            // interpoleeritud string avaldises
             string vastus1 = $"Sinu arv oli {arv} ja tema ruut on {arv * arv}";
             string vastus2 = 
                 string.Format("sinu arv oli {0} ja tema ruut on {1}", arv, arv * arv);
+            // ja sama lugu string.Format funktsiooni ja placeholderitega
 
+            // ning sama tulemuse stringi kokkuliitmise tehtega
             string vastus3 = 
                 "sinu arv oli " + arv + 
                 " ja tema ruut on " + (arv * arv).ToString();
             Console.WriteLine(vastus3);
+
+            // stringi võrdlemine
             string eesnimi = "Henn";
             string nimi1 = eesnimi + " " + "Sarv";
             string nimi2 = "Henn Sarv";
+
+            // stringi võrdlemine Equals tehtega
+            // nii võib teha C# ja nii TULEB teha Javas (seal on küll equals väikese tähega)
             if (nimi1.Equals(nimi2)) Console.WriteLine("nimed on samad");
+            else Console.WriteLine("nimed erinevad");
+
+            // c# lubab stringe ka otse võrrelda 
+            // Javas annab selline võrdlemine vale tulemuse
+            if (nimi1 == nimi2) Console.WriteLine("nimed on samad");
             else Console.WriteLine("nimed erinevad");
 
 
